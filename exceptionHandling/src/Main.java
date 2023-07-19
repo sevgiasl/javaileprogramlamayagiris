2058f86461c49678e5fed2a5f68c030c3640e08b
Sevgi Aslan <132686529+sevgiasl@users.noreply.github.com>
2023-07-19 20:44:32 +0300

    java ile programlama
    java ogrenme

--- exceptionHandling/src/Main.java
+++ exceptionHandling/src/Main.java
@@ -4,11 +4,16 @@ public class Main {
         try {
             int[] sayilar = new int[]{1, 2, 3};//dinamik gelir-veri kaynağından
             System.out.println(sayilar[5]);
-        } catch (Exception exception) {
+
+        } catch (StringIndexOutOfBoundsException exception) {
+            System.out.println(exception);
+        }catch(ArrayIndexOutOfBoundsException exception) {
             System.out.println("hata oluştu");
             System.out.println(exception);
-        } finally {
-            System.out.println("Ben her türlü çalışırım");
-        }//catch de çalışsa try da çalışsa
+        }catch(Exception exception){
+            System.out.println("Loglandı exception"+exception);
+            }finally{
+                System.out.println("Ben her türlü çalışırım");
+            }//catch de çalışsa try da çalışsa
+        }
     }
-}
